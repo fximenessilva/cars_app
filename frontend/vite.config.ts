467eb738bf4d594506017e295613de432c699393
Francisco Ximenes <francisco.ximenes@fabamaq.com>
2023-06-26 20:56:20 +0200

    [FRONTEND] - add routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3050,
+  },
+
+  resolve: {
+    alias: {
+      src: path.resolve("src/"),
+    },
   },
 });
