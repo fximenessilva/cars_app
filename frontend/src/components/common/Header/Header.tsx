94bcbe8a44ed12c5ad1d2cd6a0d3393d037b0064
Francisco Ximenes <francisco.ximenes@fabamaq.com>
2023-06-26 23:48:06 +0200

    [FRONTEND] - add header, footer, app context, generic styles and dark mode functionality

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,5 +1,31 @@
+import { useAppContext } from "@contexts/AppContext";
+import { Link } from "react-router-dom";
+
+import { THEME_NAMESPACE } from "@utils/constants";
+import styles from "./header.module.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  const { darkMode, setDarkMode } = useAppContext();
+
+  const iconClassName = `fa-solid fa-toggle-${darkMode ? "on" : "off"} ${
+    styles.icon
+  }`;
+
+  const toggleHandler = () => {
+    setDarkMode((prevState) => {
+      localStorage.setItem(THEME_NAMESPACE, JSON.stringify(!prevState));
+      return !prevState;
+    });
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <h1 className="title">AutoConnect</h1>
+      </Link>
+      <i onClick={toggleHandler} className={iconClassName} />
+    </header>
+  );
 };
 
 export default Header;
