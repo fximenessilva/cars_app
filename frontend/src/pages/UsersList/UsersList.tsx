97e320ed7495e43f0b5024e881b91745d127f6c9
Francisco Ximenes <francisco.ximenes@fabamaq.com>
2023-06-27 19:11:12 +0200

    [FRONTEND] - add pages structure

--- frontend/src/pages/UsersList/UsersList.tsx
+++ frontend/src/pages/UsersList/UsersList.tsx
@@ -1,5 +1,30 @@
+import dataUsers from "@data/users.json";
+import dataCars from "@data/cars.json";
+
+import Table from "@components/common/Table";
+import PageHead from "@components/common/PageHead/PageHead";
+import styles from "./usersList.module.scss";
+
+const { usuarios: users } = dataUsers;
+const { coches: cars } = dataCars;
+
+const data = users.map(({ coches_favoritos, id, ...props }) => ({
+  coches_favoritos: cars
+    .filter(({ id }) => coches_favoritos.includes(id))
+    .map(({ marca, nombre }) => `${marca} ${nombre}`),
+  ...props,
+}));
+const columns = [...Object.keys(users[0]).filter((el) => el !== "id"), " "];
+
 const UsersList = () => {
-  return <div>UsersList</div>;
+  const wrapperClassName = `${styles.wrapper} container`;
+
+  return (
+    <section className={wrapperClassName}>
+      <PageHead title="Users" />
+      <Table columns={columns} data={data} />
+    </section>
+  );
 };
 
 export default UsersList;
